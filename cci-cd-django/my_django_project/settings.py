99c2a9a619e5396d2c0de4fcecbfbfb50ec54020
Dwinchi <74509580+Dwinchi@users.noreply.github.com>
2021-09-07 15:56:37 -0500

    Add files via upload

--- cci-cd-django/my_django_project/settings.py
+++ cci-cd-django/my_django_project/settings.py
@@ -1,3 +1,5 @@
+import django_heroku
+
 """
 Django settings for my_django_project project.
 
@@ -119,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
